fc7beeca8fab34cf94eef94c7dc27b37076d1be1
Karan Gaba <kgaba031@gmail.com>
2022-05-03 21:30:05 -0400

    changed back to time

--- clock.js
+++ clock.js
@@ -6,18 +6,15 @@ document.addEventListener("dblclick",()=>{
 
 
 
-// function getTime(){
-//   var d= new Date();
-//   var hours=d.getHours()
-//   var format = (hours>=12)?"PM":"AM"
-//   hours=(hours>12)?hours-12:hours
-//   hours = (hours<10)?"0"+hours:hours
-//   var minutes = d.getMinutes()
-//   minutes = (minutes<10)?"0"+minutes:minutes
-//   span.textContent = hours + ":" + minutes + format;
-// }
+function getTime(){
+  var d= new Date();
+  var hours=d.getHours()
+  var format = (hours>=12)?"PM":"AM"
+  hours=(hours>12)?hours-12:hours
+  hours = (hours<10)?"0"+hours:hours
+  var minutes = d.getMinutes()
+  minutes = (minutes<10)?"0"+minutes:minutes
+  span.textContent = hours + ":" + minutes + format;
+}
 
-// setInterval(getTime,1000)
-setInterval(()=>{
-  window.location.reload()
-},5000)
+setInterval(getTime,1000)
